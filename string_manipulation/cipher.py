e879b9dd7f34755ed2e92bc0653c2cf8328d7e28
Ashika Shallow <shalashika@outlook.com>
2025-01-15 12:08:27 -0500

    started loop in caesar cipher

---
+++ string_manipulation/cipher.py
@@ -0,0 +1,12 @@
+# Caesar cipher
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+    new_index = index + shift
+    new_char = alphabet[new_index]
+    print('char:', char, 'new char:', new_char)
