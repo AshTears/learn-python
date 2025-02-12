59868456a8305fb3f1d0d22cf118fc59e372f0de
Ashika Shallow <shalashika@outlook.com>
2025-02-12 11:53:07 -0500

    Started Vigenere cipher

--- string_manipulation/cipher.py
+++ string_manipulation/cipher.py
@@ -1,6 +1,7 @@
 # Caesar cipher
 text = 'Hello Zaira'
 shift = 3
+custom_key = 'python'
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 def caesar_encode(message, offset):
@@ -16,3 +17,16 @@ def caesar_encode(message, offset):
     print('encrypted text:', encrypted_text)
 
 caesar_encode(text, shift)
+
+# Vigenere cipher
+def vigenere(message, key):
+    encrypted_text = ''
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+        
+    print('encrypted text:', encrypted_text)
