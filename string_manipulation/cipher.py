9fcbc6a2cd0001b4f50c0afa16e9dc6a30056f0a
Ashika Shallow <shalashika@outlook.com>
2025-02-19 11:32:49 -0500

    Modified vigenere cipher

--- string_manipulation/cipher.py
+++ string_manipulation/cipher.py
@@ -20,13 +20,22 @@ caesar_encode(text, shift)
 
 # Vigenere cipher
 def vigenere(message, key):
+    key_index = 0
     encrypted_text = ''
     for char in message.lower():
+        # Append space to the message
         if char == ' ':
             encrypted_text += char
         else:
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]
+            key_index+=1
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
         
-    print('encrypted text:', encrypted_text)
+    return encrypted_text
+
+print(vigenere(text, custom_key))
